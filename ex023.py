9f760ab42ae39783e93e85a1c90689e82007e1c2
Luiz Eduardo <luizeduardu234@gmail.com>
2025-03-09 21:58:40 -0300

    ex023.py

--- ex023.py
+++ ex023.py
@@ -1,3 +1,51 @@
-numero = str(input('Digite um número de 0 a 9999: '))
-if numero == [4]:
-    print('Unidade:', numero[0])
+numero = str(input('Digite um número de 0 a 9999: ')).strip()
+num = int(numero)
+
+if 0 <= num <= 9:
+    unidade = numero[0]
+    print('Este número possui: \n'
+      '{} UNIDADE(S) = {}\n'. format(unidade, unidade))
+
+if 10 == num:
+    dezena = numero[0]
+    print('Este número possui: \n'
+          '{} DEZENA(S) = {}\n'.format(dezena, dezena))
+
+if 10 < num <= 99:
+    unidade = numero[1]
+    dezena = numero[0]
+    print('Este número possui: \n'
+      '{} UNIDADE(S) = {}\n'
+      '{} DEZENA(S) = {} \n' . format(unidade, unidade, dezena, dezena + '0'))
+
+if 100 == num:
+    centena = numero[0]
+    print('Este número possui: \n'
+          '{} CENTENA(S) = {}\n'.format(centena, centena + '00'))
+
+if 100 < num <= 999:
+    unidade = numero[2]
+    dezena = numero[1]
+    centena = numero[0]
+    print('Este número possui: \n'
+      '{} UNIDADE(S) = {}\n'
+      '{} DEZENA(S) = {} \n'
+      '{} CETENA(S) = {} \n'
+          .format(unidade, unidade, dezena, dezena + '0', centena, centena + '00'))
+
+if 1000 == num:
+    milhar = numero[0]
+    print('Este número possui: \n'
+          '{} MILHAR(ES) = {}\n'.format(milhar, milhar))
+
+if 1000 < num <= 9999:
+    unidade = numero[3]
+    dezena = numero[2]
+    centena = numero[1]
+    milhar = numero[0]
+    print('Este número possui: \n'
+      '{} UNIDADE(S) = {}\n'
+      '{} DEZENA(S) = {} \n'
+      '{} CETENA(S) = {} \n'
+      '{} MILHAR(ES) = {}'
+          .format(unidade, unidade, dezena, dezena + '0', centena, centena + '00', milhar, milhar + '000'))
